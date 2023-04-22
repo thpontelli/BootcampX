0472cb29cf5094b3cf9f0d7f79103deb0b9292a2
Thiago Pontelli <thiago.pontelli@gmail.com>
2023-04-22 19:03:52 -0400

    Avoid SQL injection

--- bootcamp_app/teachers.js
+++ bootcamp_app/teachers.js
@@ -10,22 +10,22 @@ const pool = new Pool({
 const commandLineArgs = process.argv.splice(2);
 
 const queryString = `
-SELECT students.id as student_id, students.name as name, cohorts.name as cohort
-FROM students
-JOIN cohorts ON cohorts.id = students.cohort_id
-WHERE cohorts.name LIKE $1
-LIMIT $2;
+SELECT DISTINCT(t.name) as teacher, c.name as cohort
+FROM students s
+JOIN assistance_requests ar ON ar.student_id = s.id
+JOIN cohorts c ON c.id = s.cohort_id
+JOIN teachers t ON t.id = ar.teacher_id
+WHERE c.name = $1
+ORDER BY t.name;
 `
 const cohortName = commandLineArgs[0];
-const limit = commandLineArgs[1] || 5;
 
 // Store all potentially malicious values in an array.
-const values = [`%${cohortName}%`, limit];
-
+const values = [`${cohortName}`];
 
 pool.query(queryString, values)
 .then(res => {
   res.rows.forEach(user => {
-    console.log(`${user.name} has an id of ${user.student_id} and was in the ${user.cohort} cohort`);
+    console.log(`${user.cohort}: ${user.teacher}`);
   })
 });
